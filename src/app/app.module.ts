68202ab6e510255436ae6dd60beed2ba9ffd607f
Darsh789781 <darshmodi87@gmail.com>
2021-06-20 11:49:50 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
-import { StudentsComponent } from './students/students.component';
 import { SubjectComponent } from './subject/subject.component';
 import { ResponseSubjectComponent } from './response-subject/response-subject.component'
 import { SubjectService } from './shared-service/subject.service';
-import { Subject1Component } from './Subject1/subject1/subject1.component';
-import { Subject2Component } from './Subject2/subject2/subject2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReactiveFormsComponent,
-    StudentsComponent,
     SubjectComponent,
     ResponseSubjectComponent,
-    Subject1Component,
-    Subject2Component
-  
+   
     
   ],
   imports: [
